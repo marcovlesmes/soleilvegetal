ef0e69324a4ef5b2625baa94acb117e83c56509d
marcovlesmes <marcovlesmes@gmail.com>
2021-05-29 20:40:11 -0500

    Tailwind Config

---
+++ soleilvegetal/tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [
+    './storage/framework/views/*.php',
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    textColor: {
+      'primary' : '#2F6439',
+      'secondary': '#d17732',
+      'linky': '#1046CB',
+      'standart': '#0E2210'
+    },
+    boxShadow: {
+      'icon': 'rgba(6 78 59) 0 0 2px'
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {
+      display: ['group-hover'],
+    },
+  },
+  plugins: [],
+}
