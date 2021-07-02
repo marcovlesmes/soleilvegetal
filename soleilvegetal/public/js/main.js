71297672e5075d92f6d120ddf4697bd5dfa29f63
marcovlesmes <marcovlesmes@gmail.com>
2021-07-02 02:42:44 -0500

    Javascript with filter

--- soleilvegetal/public/js/main.js
+++ soleilvegetal/public/js/main.js
@@ -1,4 +1,4 @@
-window.cardItem = function () {
+window.cartItem = function () {
     return {
         count: null,
         deleteForm: null,
@@ -44,7 +44,6 @@ window.sidebar = function () {
         search_form: null,
         init: function () {
             this.search_form = this.$refs.search_form;
-            console.log('Working sidebar! :[O');
         },
         submit_form: function (e) {
             e.preventDefault();
@@ -60,16 +59,43 @@ window.sidebar = function () {
 window.filter = function () {
     return {
         filter_form: null,
-        showAutor: false,
-        showTechnique: false,
-        filteredAutors: [],
-        filteredTechniques: [],
+        autors_ul: null,
+        techniques_ul: null,
+        show_autor: false,
+        show_technique: false,
+        filtered_autors: [],
+        filtered_techniques: [],
         init: function () {
+            if (localStorage.getItem('filtered_autors')) {
+                if (window.location.pathname.search('filter') != -1) {
+                    this.filtered_autors = JSON.parse(localStorage.filtered_autors);
+                    this.filtered_techniques = JSON.parse(localStorage.filtered_techniques);
+                } else {
+                    localStorage.removeItem('filtered_autors');
+                    localStorage.removeItem('filtered_techniques');
+                }
+            }
             this.filter_form = this.$refs.filter_form;
-            console.log('Working filter... >--O');
+            this.autors_ul = this.$refs.autors;
+            this.techniques_ul = this.$refs.techniques;
+            this.updateDOM();
+        },
+        updateDOM: function() {
+            this.filtered_autors.forEach(function (filtered_autor) {
+                let autors_ul = document.querySelector('#autor_' + filtered_autor);
+                if (autors_ul) {
+                    autors_ul.checked = false;
+                }
+            });
+            this.filtered_techniques.forEach(function (filtered_technique) {
+                let techniques_ul = document.querySelector('#technique_' + filtered_technique);
+                if (techniques_ul) {
+                    techniques_ul.checked = false;
+                }
+            });
         },
         checkSwap: function (e) {
-            filterArrays = {'autor': this.filteredAutors, 'technique': this.filteredTechniques};
+            filterArrays = {'autor': this.filtered_autors, 'technique': this.filtered_techniques};
             let [type, id] = e.target.id.split('_');
             if (e.target.checked) {
                 let index = filterArrays[type].indexOf(id);
@@ -80,15 +106,16 @@ window.filter = function () {
         },
         filter: function (e) {
             e.preventDefault();
-            if (this.filteredAutors.length > 0 || this.filteredTechniques.length > 0) {
-                let filtered_autors = this.filteredAutors.length > 0 ? this.filteredAutors.join('&') : '0';
-                let url = this.filter_form.action.replace('ids', filtered_autors) + '/' + this.filteredTechniques.join('&');
+            if (this.filtered_autors.length > 0 || this.filtered_techniques.length > 0) {
+                let filtered_autors = this.filtered_autors.length > 0 ? this.filtered_autors.join('&') : '0';
+                let url = this.filter_form.action.replace('ids', filtered_autors) + '/' + this.filtered_techniques.join('&');
                 this.filter_form.action = url;
+                localStorage.setItem('filtered_autors', JSON.stringify(this.filtered_autors));
+                localStorage.setItem('filtered_techniques', JSON.stringify(this.filtered_techniques));
                 this.filter_form.submit();
             }
-
-            this.showAutor = false;
-            this.showTechnique = false;
+            this.show_autor = false;
+            this.show_technique = false;
         }
     }
 }
